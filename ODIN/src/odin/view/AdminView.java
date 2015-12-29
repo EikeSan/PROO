287c55831e6a287bbe1be5fdd2122a24470673c6
EikeSan <eikesantz@hotmail.com>
2015-12-29 20:47:37 -0300

    Versao Alpha 1.9
    GUI

---
+++ ODIN/src/odin/view/AdminView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package odin.view;
+
+/**
+ *
+ * @author eike.santiago
+ */
+public class AdminView {
+    
+}
